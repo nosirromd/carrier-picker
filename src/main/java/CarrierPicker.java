a4fe917b9ad5ea273409cf05a16d5026d6438143
dc morrison <nosirromd@gmail.com>
2020-03-07 23:56:57 +0000

    whenNoRulesAreLoadedMessageIsRejected test passed

--- src/main/java/CarrierPicker.java
+++ src/main/java/CarrierPicker.java
@@ -1,12 +1,14 @@
 
 public class CarrierPicker {
 
+    private static final String UNKNOWN_RULE = "{\"carrier\" : \"UNKNOWN\", \"destinationAddress\" : 44}";
+
     public void readCarrierRule(String ruleJson) {
 
     }
 
     public String pickCarrier(String messageJson) {
-        return null;
+        return UNKNOWN_RULE;
     }
 
 }
